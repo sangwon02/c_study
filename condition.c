76ab153e37a2c3173450f40f02dea1511800e3b1
sangwon02 <sangwon020214@gmail.com>
2021-08-03 15:12:47 +0900

    조건문 공부

--- condition.c
+++ condition.c
@@ -131,17 +131,74 @@ int main(void)
 		printf("뭐지\n");
 	}*/
 
-	srand(time(NULL));
+	/*srand(time(NULL));
 	int i = rand() % 3;
 	switch (i)
 	{
-
 	case 0: printf("가위\n"); break;
 	case 1: printf("바위\n"); break;
 	case 2: printf("보\n"); break;
 	default: printf("몰라요\n"); break;
+	}*/
 
+
+	// 초딩, 중딩, 고딩으로 나누기
+	/*int age;
+	printf("나이가 몇살이신가요?:");
+	scanf_s("%d", &age);
+	switch (age)
+	{
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+	case 13:printf("초등학생입니다.\n"); break;
+	case 14:
+	case 15:
+	case 16:printf("중학생입니다.\n"); break;
+	case 17:
+	case 18:
+	case 19:printf("고등학생입니다,\n"); break;
+	default:printf("학생이 아닙니다,\n"); break;
+	break;
+	}*/
+
+	//up and down
+	srand(time(NULL));
+	int i = rand() % 100 + 1; // 1~100수 랜덤 선택
+	int x = 5;
+	for (int k = 0; k < 5; k++)
+	{
+		int a;
+		printf("숫자를 말해주세요.(남은 기회는 %d):",x);
+		scanf_s("%d", &a);
+		x--;
+		if (a==1)
+		{
+			printf("정답입니다!\n"); 
+			break;
+		}
+		else if (x == 0)
+		{
+			printf("기회가 다 소진되었네요.\n");
+		}
+		else if (a < i)
+		{
+			printf("숫자는 더 큽니다.\n");
+		}
+		else if (a > i)
+		{
+			printf("숫자는 더 작습니다.\n");
+		}
+		else
+		{
+			printf("알 수 없습니다.");
+		}
 	}
+	printf("종료되었습니다.\n");
+
+
 
 	return 0;
 }
