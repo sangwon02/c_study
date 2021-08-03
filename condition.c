3d4a2362e703ca3d875c8365452d8d750677e1e6
sangwon02 <sangwon020214@gmail.com>
2021-08-03 14:32:15 +0900

    반복문 공부

--- condition.c
+++ condition.c
@@ -135,10 +135,12 @@ int main(void)
 	int i = rand() % 3;
 	switch (i)
 	{
+
 	case 0: printf("가위\n"); break;
 	case 1: printf("바위\n"); break;
 	case 2: printf("보\n"); break;
 	default: printf("몰라요\n"); break;
+
 	}
 
 	return 0;
